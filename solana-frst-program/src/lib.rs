abf4aaefb9abfe02046318414021daf919216d0e
satendra <ersatendrasingh1325@gmail.com>
2024-05-14 19:02:47 +0530

    added comments

--- solana-frst-program/src/lib.rs
+++ solana-frst-program/src/lib.rs
@@ -1,26 +1,29 @@
 use solana_program::{
-    account_info::AccountInfo,
-    entrypoint, // to intract with block chain
-    entrypoint::ProgramResult, // when we deploy program we get to know about the result like program is working or not
-    pubkey::Pubkey, // The address of a Solana account, as the key are in the encypted form these will work like the parser (keys will be in form of binary then it will convert to hte base58 )
-    msg
-    
+    account_info::AccountInfo,     // Importing utilities to work with account information
+    entrypoint,                    // Entry point to interact with the blockchain
+    entrypoint::ProgramResult,    // Result type to indicate success or failure of the program
+    pubkey::Pubkey,                // Data type representing the address of a Solana account
+    msg                            // Macro for printing messages to the console
 };
 
-// Now we make entry point which will work like send and get data fromt he blockchain
+// Now we define the entry point which will be called when this program is executed on the blockchain
 
 entrypoint!(process_instruction);
 
+// Function to process instructions sent to the program
 fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
+    program_id: &Pubkey,               // The program ID, which uniquely identifies this program on the blockchain
+    accounts: &[AccountInfo],          // Array of accounts associated with the program
+    instruction_data: &[u8],           // Raw instruction data sent to the program
 ) -> ProgramResult {
+    // Print a message to the console indicating that the instruction processing has started
     msg!(
         "process_instruction: {}: {} accounts, data={:?}",
         program_id,
         accounts.len(),
         instruction_data
     );
+    
+    // Return Ok(()) to indicate that the instruction processing was successful
     Ok(())
 }
